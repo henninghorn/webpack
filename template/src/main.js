4a35a73ae7800ed146ed896dde54cf0be4d85edc
Henning Horn <hh@henninghorn.dk>
2017-01-13 22:13:02 +0100

    Fixed aslant errors

--- template/src/main.js
+++ template/src/main.js
@@ -6,20 +6,20 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 // Import F7
-import Framework7 from 'framework7'
+import 'framework7'
 
 // Import F7 Vue Plugin
 import Framework7Vue from 'framework7-vue'
 
 // Import F7 iOS Theme Styles
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+import 'framework7/dist/css/framework7.ios.min.css'
+import 'framework7/dist/css/framework7.ios.colors.min.css'
 /* OR for Material Theme:
 import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
 */
 // Import F7 Icons
-import Framework7Icons from 'framework7-icons/css/framework7-icons.css'
+import 'framework7-icons/css/framework7-icons.css'
 
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
