f7aa90def79b0050b92dd36ec340feb251913347
Henning Horn <hh@henninghorn.dk>
2017-01-13 22:19:13 +0100

    Updated main template

--- template/src/App.vue
+++ template/src/App.vue
@@ -4,16 +4,23 @@
     <!-- Statusbar -->
     <f7-statusbar></f7-statusbar>
 
-    <f7-pages>
-      <f7-page>
-        <f7-block-title>
-          Welcome
-        </f7-block-title>
-        <f7-block>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae ad aut minima tempora laboriosam placeat doloribus pariatur ullam dolorem eligendi, quisquam tempore accusamus iure nostrum voluptatum omnis asperiores nemo.
-        </f7-block>
-      </f7-page>
-    </f7-pages>
+    <f7-views>
+      <f7-view main>
+        <f7-pages>
+          <f7-page navbar-through>
+            <f7-navbar>
+              <f7-nav-center>{{ name }}</f7-nav-center>
+            </f7-navbar>
+            <f7-block-title>
+              Welcome to {{ name }}
+            </f7-block-title>
+            <f7-block>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae ad aut minima tempora laboriosam placeat doloribus pariatur ullam dolorem eligendi, quisquam tempore accusamus iure nostrum voluptatum omnis asperiores nemo.
+            </f7-block>
+          </f7-page>
+        </f7-pages>
+      </f7-view>
+    </f7-views>
 
   </div>
 </template>
